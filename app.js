5a88d6b4c0b0cda6c6444ce7ca7aa6c8679ed651
NamasteSmit <smitpatel1305@gmial.com>
2024-08-24 06:56:10 -0700

    addede components folder , utils folder and app.js in root folder

---
+++ app.js
@@ -0,0 +1,4 @@
+const  num = 23;
+const num2 = 33;
+
+console.log("result " , num + num2);
---
+++ utils/constant.js
@@ -0,0 +1,7 @@
+const arr = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Alice', age: 30 },
+    { id: 3, name: 'Bob', age: 28 },
+    { id: 4, name: 'Charlie', age: 32 },
+    { id: 5, name: 'David', age: 27 }
+];
